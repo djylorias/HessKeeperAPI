1ce0a97971934da895374d44f1902ce1708b544c
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-01-17 19:33:05 +0100

    Fix entity and repository scan

--- src/main/java/ludo/bernard/HessKeeperAPI/HessKeeperApiApplication.java
+++ src/main/java/ludo/bernard/HessKeeperAPI/HessKeeperApiApplication.java
@@ -2,8 +2,12 @@ package ludo.bernard.HessKeeperAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("ludo.bernard.HessKeeperAPI.repositories.*")
+@EntityScan("ludo.bernard.HessKeeperAPI.dto.*")
 public class HessKeeperApiApplication {
 
 	public static void main(String[] args) {
