6653b0ecbf5d2830c8018f983995a2d08518f0fe
Bernard ludovic <ludovic.bernard.etu@univ-lille.fr>
2025-01-17 14:41:33 +0100

    Pushing blank maven project with dependencies

---
+++ src/main/java/ludo/bernard/HessKeeperAPI/HessKeeperApiApplication.java
@@ -0,0 +1,13 @@
+package ludo.bernard.HessKeeperAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HessKeeperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HessKeeperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ludo/bernard/HessKeeperAPI/HessKeeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ludo.bernard.HessKeeperAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HessKeeperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
