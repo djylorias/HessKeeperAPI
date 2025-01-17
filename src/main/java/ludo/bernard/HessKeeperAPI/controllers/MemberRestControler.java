4b9d22fba0c1bca1139040970bb377f8576b938e
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-01-17 23:48:51 +0100

    Add method to return all members

--- src/main/java/ludo/bernard/HessKeeperAPI/controllers/MemberRestControler.java
+++ src/main/java/ludo/bernard/HessKeeperAPI/controllers/MemberRestControler.java
@@ -1,8 +1,13 @@
 package ludo.bernard.HessKeeperAPI.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ludo.bernard.HessKeeperAPI.dto.user.Member;
 import ludo.bernard.HessKeeperAPI.repositories.MemberRepository;
 
 @RestController
@@ -10,5 +15,10 @@ public class MemberRestControler {
     
     @Autowired
     private MemberRepository repository;
+
+    @GetMapping("/members")
+    public List<Member> selectAll(){
+        return (ArrayList<Member>) repository.findAll();
+    }
 
 }
