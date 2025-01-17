bc80d073ab9f9287d108a5b7f4071e9f2ee99ee4
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-01-17 23:46:39 +0100

    Remove entity and repositories scanners

--- src/main/java/ludo/bernard/HessKeeperAPI/HessKeeperApiApplication.java
+++ src/main/java/ludo/bernard/HessKeeperAPI/HessKeeperApiApplication.java
@@ -2,12 +2,10 @@ package ludo.bernard.HessKeeperAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("ludo.bernard.HessKeeperAPI.repositories.*")
-@EntityScan("ludo.bernard.HessKeeperAPI.dto.*")
+// @EnableJpaRepositories("ludo.bernard.HessKeeperAPI.repositories.*")
+// @EntityScan("ludo.bernard.HessKeeperAPI.dto.*")
 public class HessKeeperApiApplication {
 
 	public static void main(String[] args) {
