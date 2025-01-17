76990bdd27fb0b5458fe3d443c8352bc59835310
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-01-18 00:02:45 +0100

    Create some members imports

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO member(email, password, firstname, lastname) VALUES('ludo.bernard@gmail.com', '123456', 'Ludo', 'Bernard');
+INSERT INTO member(email, password, firstname, lastname) VALUES('michel.bernard@gmail.com', 'motdepasse', 'Michel', 'Bernard');
+INSERT INTO member(email, password, firstname, lastname) VALUES('lucette.bernard@gmail.com', 'lulu', 'Lucette', 'Bernard');
+INSERT INTO member(email, password, firstname, lastname) VALUES('melanie.bernard@gmail.com', 'english', 'Mélanie', 'Bernard');
