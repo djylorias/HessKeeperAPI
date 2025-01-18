d61722841d3d6bb92ccd9007770f0c6e27d6530c
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-01-18 20:15:49 +0100

    Fix imports with good table name
    Rename INSERT INTO member to INSERT INTO users

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO member(email, password, firstname, lastname) VALUES('ludo.bernard@gmail.com', '123456', 'Ludo', 'Bernard');
-INSERT INTO member(email, password, firstname, lastname) VALUES('michel.bernard@gmail.com', 'motdepasse', 'Michel', 'Bernard');
-INSERT INTO member(email, password, firstname, lastname) VALUES('lucette.bernard@gmail.com', 'lulu', 'Lucette', 'Bernard');
-INSERT INTO member(email, password, firstname, lastname) VALUES('melanie.bernard@gmail.com', 'english', 'Mélanie', 'Bernard');
+INSERT INTO users(email, password, firstname, lastname) VALUES('ludo.bernard@gmail.com', '123456', 'Ludo', 'Bernard');
+INSERT INTO users(email, password, firstname, lastname) VALUES('michel.bernard@gmail.com', 'motdepasse', 'Michel', 'Bernard');
+INSERT INTO users(email, password, firstname, lastname) VALUES('lucette.bernard@gmail.com', 'lulu', 'Lucette', 'Bernard');
+INSERT INTO users(email, password, firstname, lastname) VALUES('melanie.bernard@gmail.com', 'english', 'Mélanie', 'Bernard');
