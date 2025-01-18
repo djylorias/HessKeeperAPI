13fd3ac9f686537436efa8a1860ed1fbc3010f36
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-01-18 20:17:10 +0100

    Fixing import for user entity

--- src/main/java/ludo/bernard/HessKeeperAPI/controllers/UserRestControler.java
+++ src/main/java/ludo/bernard/HessKeeperAPI/controllers/UserRestControler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ludo.bernard.HessKeeperAPI.dto.user.User;
+import ludo.bernard.HessKeeperAPI.dto.User;
 import ludo.bernard.HessKeeperAPI.repositories.UserRepository;
 
 @RestController
--- src/main/java/ludo/bernard/HessKeeperAPI/repositories/UserRepository.java
+++ src/main/java/ludo/bernard/HessKeeperAPI/repositories/UserRepository.java
@@ -5,7 +5,7 @@ package ludo.bernard.HessKeeperAPI.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
-import ludo.bernard.HessKeeperAPI.dto.user.User;
+import ludo.bernard.HessKeeperAPI.dto.User;
 
 @Component
 public interface UserRepository extends CrudRepository<User, String>{}
