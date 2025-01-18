c8a1e8ea2c860e891bd4fd681b39facaab301697
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-01-18 20:14:34 +0100

    Change member rest controler to an user one

---
+++ src/main/java/ludo/bernard/HessKeeperAPI/controllers/UserRestControler.java
@@ -0,0 +1,30 @@
+package ludo.bernard.HessKeeperAPI.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ludo.bernard.HessKeeperAPI.dto.user.User;
+import ludo.bernard.HessKeeperAPI.repositories.UserRepository;
+
+@RestController
+public class UserRestControler {
+    
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/members")
+    public List<User> selectAll(){
+        return (ArrayList<User>) repository.findAll();
+    }
+
+    @PostMapping("/members/createMember")
+    public User creatreMember(User m){
+        return repository.save(m);
+    }
+
+}
