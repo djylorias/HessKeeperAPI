7c5a00c159b0dc7620bac83548599d3772a5c378
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-01-19 22:17:03 +0100

    Add inserts for alteration types

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,11 @@
+-- import defaults users
 INSERT INTO users(email, password, firstname, lastname) VALUES('ludo.bernard@gmail.com', '123456', 'Ludo', 'Bernard');
 INSERT INTO users(email, password, firstname, lastname) VALUES('michel.bernard@gmail.com', 'motdepasse', 'Michel', 'Bernard');
 INSERT INTO users(email, password, firstname, lastname) VALUES('lucette.bernard@gmail.com', 'lulu', 'Lucette', 'Bernard');
 INSERT INTO users(email, password, firstname, lastname) VALUES('melanie.bernard@gmail.com', 'english', 'Mélanie', 'Bernard');
+
+-- import defaults alteration types
+INSERT INTO alteration_type VALUES('annual', 'ANNUAL');
+INSERT INTO alteration_type VALUES('monthly', 'MONTHLY');
+INSERT INTO alteration_type VALUES('weekly', 'WEEKLY');
+INSERT INTO alteration_type VALUES('daily', 'DAILY');
